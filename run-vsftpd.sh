3d11ae1dd8436fc3b75f2e253bd35966804d8683
adamlien <adamlien329@gmail.com>
2019-06-22 12:28:48 +0800

    init

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -24,15 +24,15 @@ chown -R ftp:ftp /home/vsftpd/
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
-# Set passive mode parameters:
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
-fi
+## Set passive mode parameters:
+#if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+#    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+#fi
 
 echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+#echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
 echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
 echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
